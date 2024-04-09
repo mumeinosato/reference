0f78a7a3ae61c56c67210bf14ed6fba665b6c2be
mumeinosato <mumeinosato@mumeinosato@gmail.com>
2024-04-09 13:15:11 +0900

    くそださボタン

--- frontend/src/views/List.vue
+++ frontend/src/views/List.vue
@@ -1,4 +1,11 @@
 <template>
+  <div>
+    <div>
+      <div class="toggle_button">
+      <input id="toggle" class="toggle_input" type='checkbox' />
+      <label for="toggle" class="toggle_label"/>
+  </div>
+    </div>
   <div class="box">
     <draggable :list="re" @change="update">
     <div v-for="(item, index) in re" :key="index">
@@ -11,11 +18,13 @@
     </div>
   </draggable>
   </div>
+</div>
 </template>
         
-        <script>
-import { list, edit_list } from "../assets/script/api";
+<script>
+import { list, edit_list, edit, login } from "../assets/script/api";
 import { VueDraggableNext } from 'vue-draggable-next'
+import { useStore } from "../assets/script/store"
 
 export default {
   components: {
@@ -25,6 +34,8 @@ export default {
     return {
       re: [], // データを保持する配列
       type: null,
+      edit: false,
+      login: false,
     };
   },
   async mounted() {
@@ -33,6 +44,9 @@ export default {
     this.re = await list(lang, this.type, this.$route.params.group);
 
     this.re.sort((a, b) => a.list - b.list);
+
+    const store = useStore();
+    this.login = store.getLogin();
   },
   methods: {
     async update(){
@@ -63,5 +77,60 @@ p:hover {
   display: flex;
   flex-direction: column;
   flex-wrap: wrap;
+}
+
+
+
+.toggle_input {
+  position: absolute;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  z-index: 5;
+  opacity: 0;
+  cursor: pointer;
+}
+
+.toggle_label {
+  width: 75px;
+  height: 35px;
+  background: #ccc;
+  position: relative;
+  display: inline-block;
+  border-radius: 40px;
+  transition: 0.4s;
+  box-sizing: border-box;
+}
+
+.toggle_label:after {
+  content: "";
+  position: absolute;
+  width: 35px;
+  height: 35px;
+  border-radius: 100%;
+  left: 0;
+  top: 0;
+  z-index: 2;
+  background: #fff;
+  box-shadow: 0 0 5px rgba(0, 0, 0, 0.2);
+  transition: 0.4s;
+}
+
+.toggle_input:checked + .toggle_label {
+  background-color: #4BD865;
+}
+
+.toggle_input:checked + .toggle_label:after {
+  left: 40px;
+}
+
+.toggle_button {
+  position: absolute;
+  right: 0;
+  width: 75px;
+  height: 35px;
+  margin: auto;
+  margin-bottom: 50px;
 }
 </style>
