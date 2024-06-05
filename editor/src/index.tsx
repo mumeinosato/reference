0ca1898333c993dfe3dbca2eda2f715fc8e6a525
mumeinosato <66110797+mumeinosato@users.noreply.github.com>
2024-06-05 12:54:27 +0900

    tsxを追加

---
+++ editor/src/index.tsx
@@ -0,0 +1,14 @@
+import React from "react"
+import ReactDOM from "react-dom"
+import "./index.css"
+import App from "./App"
+
+const root = ReactDOM.creare.Root(
+    document.getElementById('root') as HTMLElement
+);
+
+root.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+);
